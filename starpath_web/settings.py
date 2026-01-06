d1514245c17a676a76a5b2b3c36d7ce2b37a2ba5
soportestarpathai <soporte@starpathai.mx>
2026-01-05 18:27:25 -0600

    Feat: Landing Page

--- starpath_web/settings.py
+++ starpath_web/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mi_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'starpath_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- starpath_web/urls.py
+++ starpath_web/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mi_app.views.landing_page.landing_page_views import LandingPage
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path("", LandingPage.as_view(), name="home"),
 ]
